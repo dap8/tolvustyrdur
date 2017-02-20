3aed67d4b0384783f54d5a16692fd35b2127ea12
Óskar Ólafsson <Notandi@users.noreply.github.com>
2017-02-20 13:37:42 +0000

    Búinn að bæta við myndum

--- vefsida/routes/hiddenroute.js
+++ vefsida/routes/hiddenroute.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('hiddenroute', { title: 'Express' });
+  res.render('hiddenroute', { title: 'Dagbók' });
 });
 
 module.exports = router;
