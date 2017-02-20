999d2a353eb1a75f26dade2674b97a10fa9d96f5
Óskar Ólafsson <Notandi@users.noreply.github.com>
2017-02-20 13:20:44 +0000

    Búinn að búa til hiddenroute

--- vefsida/app.js
+++ vefsida/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var hiddenRoute = require('./routes/hiddenroute');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/skkskskseiieielasdf',hiddenRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
